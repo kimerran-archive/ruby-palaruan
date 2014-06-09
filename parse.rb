5a1b41be4a4f67ba1b8572a3b72df39768cba180
kimerran <kimerran@users.noreply.github.com>
2014-06-09 10:28:49 +0800

    Update parse.rb

--- parse.rb
+++ parse.rb
@@ -1,10 +1,9 @@
 =begin
-  * Name		: parse.rb
-  * Description : utility script for kimeblog to
-				  generate all.json
-  * Author		: mh.neri@gmail.com
-  * Date		: 8-June-2014
-  * License		: Unlicensed
+  * Name: parse.rb
+  * Description: utility script for kimeblog to generate all.json
+  * Author: mh.neri@gmail.com
+  * Date: 8-June-2014
+  * License: Unlicensed
 =end
 out_f = File.new("all.json", "w")
 out_f.puts("[")
@@ -14,7 +13,7 @@ Dir.foreach("posts") { |x|
 		contents = File.read("#{Dir.pwd}/posts/#{x}")	
 		meta =  /%(\{[^\}]+\})%[^*]+/.match(contents).captures		
 		out_f.puts(meta)
-		out_f.puts(",")
+		out_f.print(",")
 	end
 }
 out_f.puts("]")
